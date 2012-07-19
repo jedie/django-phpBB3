7948a92f7a4155834477777c1f887c913f20a44d
JensDiemer <git@jensdiemer.de>
2012-07-19 18:08:13 +0200

    commit current migration script state

--- django_phpBB3/management/commands/phpbb2djangobb.py
+++ django_phpBB3/management/commands/phpbb2djangobb.py
@@ -8,13 +8,15 @@
     :copyleft: 2012 by the django-phpBB3 team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
+import datetime
+import time
 if __name__ == "__main__":
     import os
     import sys
     os.environ["DJANGO_SETTINGS_MODULE"] = "django_phpBB3_project.settings"
     from django.core import management
     management.call_command("phpbb2djangobb",
-        clear_tables=True,
+#        clear_tables=True,
         cleanup_users=3
     )
     sys.exit()
@@ -24,10 +26,14 @@ from optparse import make_option
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 
-from djangobb_forum.models import Category, Forum, Profile, TZ_CHOICES
+from postmarkup import render_bbcode
+from djangobb_forum.models import Category, Forum, Profile, TZ_CHOICES, Post, Topic
 
 from django_phpBB3.models import Forum as phpbb_Forum
+from django_phpBB3.models import Topic as phpbb_Topic
+from django_phpBB3.models import Group as phpbb_Group
 from django_phpBB3.models import User as phpbb_User
+from django_phpBB3.models import Post as phpbb_Post
 
 
 class Command(BaseCommand):
@@ -54,10 +60,18 @@ class Command(BaseCommand):
         if clear_tables:
             self.clear_tables()
 
-        self.migrate_forums()
-
         cleanup_users = int(options.get("cleanup_users"))
-        self.migrate_users(cleanup_users)
+        moderator_groups = phpbb_Group.objects.filter(
+            name__in=["ADMINISTRATORS", "GLOBAL_MODERATORS"]
+        )
+        user_dict, moderators = self.migrate_users(cleanup_users, moderator_groups)
+
+        forum_dict = self.migrate_forums(moderators)
+
+        topic_dict, last_post_dict = self.migrate_topic(user_dict, forum_dict)
+        post_id_dict = self.migrate_posts(user_dict, topic_dict, last_post_dict)
+
+        self.set_last_post(post_id_dict, last_post_dict)
 
         self.stdout.write("\nmigration done.\n")
 
@@ -68,13 +82,91 @@ class Command(BaseCommand):
         Profile.objects.all().delete()
         #User.objects.all().exclude(username="test").delete()
 
-    def migrate_forums(self):
+    def migrate_users(self, cleanup_users, moderator_groups):
+        self.stdout.write("Migrate phpbb_forum users...\n")
+
+        moderators = []
+        user_dict = {}
+        phpbb_users = phpbb_User.objects.all()
+        for phpbb_user in phpbb_users:
+            if not phpbb_user.posts:
+                # Only users with has no posts can be skip.
+                if cleanup_users >= 1:
+                    if not phpbb_user.email:
+                        self.stdout.write("\t * Skip '%s' (no email)\n" % phpbb_user)
+                        continue
+                if cleanup_users >= 2:
+                    if not phpbb_user.lastvisit:
+                        self.stdout.write("\t * Skip '%s' (no lastvisit)\n" % phpbb_user)
+                        continue
+                if cleanup_users >= 3:
+                    if not phpbb_user.posts:
+                        self.stdout.write("\t * Skip '%s' (no posts)\n" % phpbb_user)
+                        continue
+
+            last_login = phpbb_user.lastvisit_datetime()
+            if not last_login:
+                # can't be None in User model:
+                last_login = datetime.datetime(year=datetime.MINYEAR, month=1, day=1)
+
+            django_user, created = User.objects.get_or_create(
+                username=phpbb_user.username,
+                defaults={
+                    "email":phpbb_user.email,
+                    "is_staff": False,
+                    "is_active": False,
+                    "is_superuser": False,
+                    "last_login": last_login,
+                    "date_joined": phpbb_user.registration_datetime(),
+                }
+            )
+            if created:
+                self.stdout.write("\tUser '%s' created.\n" % django_user)
+                django_user.set_unusable_password()
+                django_user.save()
+            else:
+                self.stdout.write("\tUser '%s' exists.\n" % django_user)
+
+            if phpbb_user.group in moderator_groups:
+                self.stdout.write("\t *** Mark user '%s' as global forum moderator\n" % phpbb_user)
+                moderators.append(django_user)
+
+            user_dict[phpbb_user.id] = django_user
+
+            tz = TZ_CHOICES[int(phpbb_user.timezone)][0]
+            #print tz
+
+            user_profile, created = Profile.objects.get_or_create(
+                user=django_user,
+                defaults={
+                    "site": phpbb_user.website,
+                    "signature": phpbb_user.sig,
+                    #"signature_html": phpbb_user.sig,
+
+                    "time_zone": tz,
+
+                    "jabber":phpbb_user.jabber,
+                    "icq":phpbb_user.icq,
+                    "msn":phpbb_user.msnm,
+                    "aim":phpbb_user.aim,
+
+                }
+            )
+            if created:
+                self.stdout.write("\t - User profile for '%s' created.\n" % django_user)
+            else:
+                self.stdout.write("\t - User profile for '%s' exists.\n" % django_user)
+
+        return user_dict, moderators
+
+    def migrate_forums(self, moderators):
         self.stdout.write("Migrate phpbb_forum entries...\n")
 
         phpbb_forums = phpbb_Forum.objects.all()
 
         # Create Categories
         category_dict = {}
+        forum_dict = {}
         for phpbb_forum in phpbb_forums:
             #print phpbb_forum
             try:
@@ -114,8 +206,7 @@ class Command(BaseCommand):
                 defaults={
                     "category":category,
                     "description":phpbb_forum.forum_desc,
-                    #    position             
-                    #    moderators
+                    #    position
                     #    post_count
                     #    topic_count
                     #    last_post
@@ -126,90 +217,130 @@ class Command(BaseCommand):
             else:
                 self.stdout.write("\tForum '%s' exists.\n" % obj)
 
-    def migrate_users(self, cleanup_users):
-        self.stdout.write("Migrate phpbb_forum users...\n")
+            forum_dict[phpbb_forum.id] = obj
 
-        phpbb_users = phpbb_User.objects.all()
-        for phpbb_user in phpbb_users:
-            if cleanup_users >= 1:
-                if not phpbb_user.email:
-                    self.stdout.write("\t * Skip '%s' (no email)\n" % phpbb_user)
-                    continue
-            if cleanup_users >= 2:
-                if not phpbb_user.lastvisit:
-                    self.stdout.write("\t * Skip '%s' (no lastvisit)\n" % phpbb_user)
-                    continue
-            if cleanup_users >= 3:
-                if not phpbb_user.posts:
-                    self.stdout.write("\t * Skip '%s' (no posts)\n" % phpbb_user)
-                    continue
+            for moderator in moderators:
+                obj.moderators.add(moderator)
+            obj.save()
+            self.stdout.write("\t - moderators: %s\n" % obj.moderators.all())
 
-            django_user, created = User.objects.get_or_create(
-                username=phpbb_user.username,
+        return forum_dict
+
+
+    def migrate_topic(self, user_dict, forum_dict):
+        self.stdout.write("Migrate phpBB topic entries...\n")
+
+        last_post_dict = {}
+        topic_dict = {}
+        topics = phpbb_Topic.objects.all().order_by("time")
+        total = topics.count()
+        count = 0
+        next_status = time.time() + 0.25
+        for topic in topics:
+            count += 1
+            if time.time() > next_status:
+                self.stdout.write("\r\t%i/%i topics migrated...          " % (count, total))
+                next_status = time.time() + 1
+
+            user = user_dict[topic.poster.id]
+            forum = forum_dict[topic.forum.id]
+
+            if topic.type in (1, 2):
+                # POST_NORMAL(0), POST_STICKY(1), POST_ANNOUNCE(2) or POST_GLOBAL(3)
+                # FIXME: what is POST_GLOBAL ?
+                sticky = True
+            else:
+                sticky = False
+
+            if topic.status == 1:
+                # ITEM_UNLOCKED(0), ITEM_LOCKED(1) or ITEM_MOVED(2)
+                # FIXME: What is ITEM_MOVED ?
+                closed = True
+            else:
+                closed = False
+
+            obj, created = Topic.objects.get_or_create(
+                forum=forum,
+                user=user,
+                name=topic.title,
                 defaults={
-                    "email":phpbb_user.email,
-                    "is_staff": False,
-                    "is_active": False,
-                    "is_superuser": False,
-                    "last_login": phpbb_user.lastvisit_datetime(),
-                    "date_joined": phpbb_user.registration_datetime(),
+                    "created": topic.create_datetime(),
+                    #"updated":,
+                    "views": topic.views,
+                    "sticky": sticky,
+                    "closed": closed,
+                    #"subscribers":, # FIXME: ForumWatch / TopicWatch models are unsupported
+                    "post_count": topic.replies_real,
+                    "last_post": None, # will be set in migrate_posts()
                 }
             )
-            if created:
-                self.stdout.write("\tUser '%s' created.\n" % django_user)
-                django_user.set_unusable_password()
-                django_user.save()
+#            if created:
+#                self.stdout.write("\tTopic '%s' created.\n" % obj)
+#            else:
+#                self.stdout.write("\tTopic '%s' exists.\n" % obj)
+
+            last_post_dict[obj] = topic.last_post.id
+            topic_dict[topic.id] = obj
+
+        self.stdout.write("\n *** %i topics migrated.\n" % count)
+        return topic_dict, last_post_dict
+
+
+    def migrate_posts(self, user_dict, topic_dict, last_post_dict):
+        self.stdout.write("Migrate phpBB posts entries...\n")
+
+        post_id_dict = {}
+
+        posts = phpbb_Post.objects.all().order_by("time")
+        total = posts.count()
+        count = 0
+        next_status = time.time() + 0.25
+        for phpbb_post in posts:
+            count += 1
+            if time.time() > next_status:
+                self.stdout.write("\r\t%i/%i posts migrated...          " % (count, total))
+                next_status = time.time() + 1
+
+            if phpbb_post.has_attachment():
+                self.stdout.write("\n\t *** TODO: transfer attachment!\n")
+
+            topic = topic_dict[phpbb_post.topic.id]
+            user = user_dict[phpbb_post.poster.id]
+
+            if phpbb_post.edit_user == 0:
+                updated_by = None
             else:
-                self.stdout.write("\tUser '%s' exists.\n" % django_user)
+                updated_by = user_dict[phpbb_post.edit_user]
 
-
-            tz = TZ_CHOICES[int(phpbb_user.timezone)][0]
-            #print tz
-
-            user_profile, created = Profile.objects.get_or_create(
-                user=django_user,
+            obj, created = Post.objects.get_or_create(
+                topic=topic,
+                user=user,
                 defaults={
-                    "site": phpbb_user.website,
-                    "signature": phpbb_user.sig,
-                    #"signature_html": phpbb_user.sig,
-
-                    "time_zone": tz,
-
-                    "jabber":phpbb_user.jabber,
-                    "icq":phpbb_user.icq,
-                    "msn":phpbb_user.msnm,
-                    "aim":phpbb_user.aim,
-
+                    "created": phpbb_post.create_datetime(),
+                    "updated":phpbb_post.updated_datetime(),
+                    "updated_by": updated_by,
+                    "markup": "bbcode",
+                    "body": phpbb_post.text,
+                    #"body_html": html, # would be generated in save()
+                    "user_ip": phpbb_post.poster_ip,
                 }
             )
-            if created:
-                self.stdout.write("\tUser profile for '%s' created.\n" % django_user)
-            else:
-                self.stdout.write("\tUser profile for '%s' exists.\n" % django_user)
+            post_id_dict[phpbb_post.id] = obj.id
 
+        self.stdout.write("\n *** %i posts migrated.\n" % count)
+        return post_id_dict
 
-"""
-class Profile(models.Model):
-    user = AutoOneToOneField(User, related_name='forum_profile', verbose_name=_('User'))
-    status = models.CharField(_('Status'), max_length=30, blank=True)
-    site = models.URLField(_('Site'), verify_exists=False, blank=True)
-    jabber = models.CharField(_('Jabber'), max_length=80, blank=True)
-    icq = models.CharField(_('ICQ'), max_length=12, blank=True)
-    msn = models.CharField(_('MSN'), max_length=80, blank=True)
-    aim = models.CharField(_('AIM'), max_length=80, blank=True)
-    yahoo = models.CharField(_('Yahoo'), max_length=80, blank=True)
-    location = models.CharField(_('Location'), max_length=30, blank=True)
-    signature = models.TextField(_('Signature'), blank=True, default='', max_length=forum_settings.SIGNATURE_MAX_LENGTH)
-    signature_html = models.TextField(_('Signature'), blank=True, default='', max_length=forum_settings.SIGNATURE_MAX_LENGTH)
-    time_zone = models.FloatField(_('Time zone'), choices=TZ_CHOICES, default=float(forum_settings.DEFAULT_TIME_ZONE))
-    language = models.CharField(_('Language'), max_length=5, default='', choices=settings.LANGUAGES)
-    avatar = ExtendedImageField(_('Avatar'), blank=True, default='', upload_to=forum_settings.AVATARS_UPLOAD_TO, width=forum_settings.AVATAR_WIDTH, height=forum_settings.AVATAR_HEIGHT)
-    theme = models.CharField(_('Theme'), choices=THEME_CHOICES, max_length=80, default='default')
-    show_avatar = models.BooleanField(_('Show avatar'), blank=True, default=True)
-    show_signatures = models.BooleanField(_('Show signatures'), blank=True, default=True)
-    show_smilies = models.BooleanField(_('Show smilies'), blank=True, default=True)
-    privacy_permission = models.IntegerField(_('Privacy permission'), choices=PRIVACY_CHOICES, default=1)
-    markup = models.CharField(_('Default markup'), max_length=15, default=forum_settings.DEFAULT_MARKUP, choices=MARKUP_CHOICES)
-    post_count = models.IntegerField(_('Post count'), blank=True, default=0)
+    def set_last_post(self, post_id_dict, last_post_dict):
+        self.stdout.write("\nTODO!\n")
+#        self.stdout.write("set last post information...\n")
+#
+#        for topic, last_post_id in last_post_dict.items():
+#            last_post_phpbb_id = post_id_dict[last_post_id]
+#            
+#            last_post = 
+#
+#            print "set last post %s to topic %s" % (last_post, topic)
+#
+#            topic.last_post = last_post
+#            topic.save()
 
-"""
